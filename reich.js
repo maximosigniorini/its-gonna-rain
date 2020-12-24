e2040c2aae6740256e4c0a661e780b6d45f02404
Maximo Signiorini <maximosigniorini97@gmail.com>
2020-12-24 20:35:28 -0300

    First wavesurfer tests

--- reich.js
+++ reich.js
@@ -1,31 +1,65 @@
-let audioContext = new AudioContext();
+var wavesurfer = WaveSurfer.create({
+    container: document.querySelector('#waveform1'),
+    backend: 'MediaElement',
+    plugins: [
+        WaveSurfer.regions.create({
+            regionsMinLength: 1,
+            regions: [
+                {
+                    start: 3,
+                    end: 4,
+                    loop: true,
+                    color: 'rgba(0, 100, 255, 0.5)'
+                }
+            ],
+            dragSelection: {
+                slop: 5
+            }
+        })
+    ]
+});
 
-function startLoop(audioBuffer, pan = 0, rate = 1) {
-    let sourceNode = audioContext.createBufferSource();
-    let pannerNode = audioContext.createStereoPanner();
+var wavesurfer2 = WaveSurfer.create({
+    container: document.querySelector('#waveform2'),
+    backend: 'MediaElement',
+    plugins: [
+        WaveSurfer.regions.create({
+            regionsMinLength: 1,
+            regions: [
+                {
+                    start: 3,
+                    end: 4,
+                    loop: true,
+                    color: 'rgba(0, 100, 255, 0.5)'
+                }
+            ],
+            dragSelection: {
+                slop: 5
+            }
+        })
+    ]
+});
 
-    sourceNode.buffer = audioBuffer;
-    sourceNode.loop = true;
-    sourceNode.loopStart = 2.98;
-    sourceNode.loopEnd = 3.92;
-    sourceNode.playbackRate.value = rate;
-    pannerNode.pan.value = pan;
+wavesurfer.load('rain.mp3');
+wavesurfer2.load('rain.mp3');
+wavesurfer2.setPlaybackRate(1.002)
 
-    sourceNode.connect(pannerNode);
-    pannerNode.connect(audioContext.destination);
+wavesurfer.panner = wavesurfer.backend.ac.createStereoPanner();
+wavesurfer2.panner = wavesurfer2.backend.ac.createStereoPanner();
+wavesurfer.panner.pan.value = 1
+wavesurfer2.panner.pan.value = -1
+wavesurfer.backend.setFilter(wavesurfer.panner)
 
-    sourceNode.start(0, 2.98);
-}
+let region = Object.values(wavesurfer.regions.list)[0];
+let region2 = Object.values(wavesurfer2.regions.list)[0];
 
-fetch('rain.mp3')
-    .then(response => response.arrayBuffer())
-    .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-    .then(audioBuffer => {
-        startLoop(audioBuffer, -1)
-        startLoop(audioBuffer, 1, 1.002)
-    })
-    .catch(e => console.error(e));
+wavesurfer.on('ready', function () {
+    region.play();
+});
 
+wavesurfer2.on('ready', function () {
+    //region2.play();
+});
 
 
 
