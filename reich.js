ba7d5b41d5238decb9a1f1a3cf80b14672d887b6
Maximo Signiorini <maximosigniorini97@gmail.com>
2020-12-26 01:23:58 -0300

    Starting Ui

--- reich.js
+++ reich.js
@@ -1,5 +1,9 @@
 let wavesurfer = []
 let region = []
+let btn = document.querySelector('button')
+let flag = false
+
+
 
 for (let i = 0; i < 2; i++) {
     wavesurfer[i] = WaveSurfer.create({
@@ -13,7 +17,8 @@ for (let i = 0; i < 2; i++) {
                         start: 3,
                         end: 4,
                         loop: true,
-                        color: 'rgba(0, 100, 255, 0.5)'
+                        color: 'rgba(0, 100, 255, 0.5)',
+                        resize: false
                     }
                 ],
                 dragSelection: {
@@ -27,7 +32,7 @@ for (let i = 0; i < 2; i++) {
     wavesurfer[i].panner = wavesurfer[i].backend.ac.createStereoPanner();
 
     if (i === 1) {
-        wavesurfer[1].setPlaybackRate(1.01)
+        wavesurfer[1].setPlaybackRate(1.002)
         wavesurfer[1].panner.pan.value = -1
     } else {
         wavesurfer[0].panner.pan.value = 1
@@ -36,11 +41,23 @@ for (let i = 0; i < 2; i++) {
     wavesurfer[i].backend.setFilter(wavesurfer[i].panner)
 
     region.push(Object.values(wavesurfer[i].regions.list)[0]);
-
-    wavesurfer[i].on('ready', function () {
-        region[i].play();
-    });
 }
+
+btn.addEventListener('click', () => {
+    flag = !flag;
+
+    if (flag) {
+        for (let j = 0; j < 2; j++) {
+            region[j].play();
+            btn.innerHTML = 'Pause'
+        }
+    } else {
+        for (let k = 0; k < 2; k++) {
+            wavesurfer[k].pause()
+            btn.innerHTML = 'Start'
+        }
+    }
+})
 
 
 
